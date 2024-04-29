cf512656bfa91356090924be3f2c93662ae32dc2
alex <iamalexseo@gmail.com>
2024-04-29 21:05:20 +0900

    코틀린

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:pharmmatch_app/screens/login_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(const MyApp());
+// }
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Pharmmatch',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-      ),
-      home: const HomeScreen(),
-    );
+    return const LoginScreen();
   }
 }
