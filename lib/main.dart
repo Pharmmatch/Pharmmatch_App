884424e46fd9a0fd628e7307a996c5821d93e5d5
alex <iamalexseo@gmail.com>
2024-05-13 17:40:37 +0900

    회원가입 로그인 연결

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/',
+      initialRoute: '/login',
       routes: {
         '/': (context) => const HomeScreen(),
         '/login': (context) => const LoginScreen(),
